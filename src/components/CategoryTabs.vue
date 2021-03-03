10d0530548aa257d4f0f6c575623f742667189a0
Jayant Dhawan <jayantdhawan95@gmail.com>
2021-03-03 21:56:32 +0530

    change to dropdown menu if more than 5 categories

--- src/components/CategoryTabs.vue
+++ src/components/CategoryTabs.vue
@@ -1,13 +1,25 @@
 <template>
   <h3 is="sui-header" block>
     <sui-header-content>
-      <sui-button @click="setCategory('')">All</sui-button>
-      <sui-button
-        @click="setCategory(category)"
-        v-for="(category, index) in categories"
-        :key="index"
-        >{{ category.toUpperCase() }}</sui-button
-      >
+      <div v-if="categories.length <= 5">
+        <sui-button @click="setCategory('')">All</sui-button>
+        <sui-button
+          @click="setCategory(category)"
+          v-for="(category, index) in categories"
+          :key="index"
+          >{{ category.toUpperCase() }}</sui-button
+        >
+      </div>
+      <div v-else>
+        <sui-dropdown
+          fluid
+          :options="categoriesObj"
+          placeholder="Select Category"
+          search
+          selection
+          v-model="current"
+        />
+      </div>
     </sui-header-content>
   </h3>
 </template>
@@ -18,6 +30,25 @@ import { mapActions } from "vuex";
 export default {
   name: "CategoryTabs",
   props: ["categories"],
+  data() {
+    return {
+      current: ""
+    };
+  },
+  watch: {
+    current(val) {
+      this.setCategory(val);
+    }
+  },
+  computed: {
+    categoriesObj() {
+      const obj = [{ text: "ALL", value: "" }];
+      this.categories.forEach(element => {
+        obj.push({ text: element.toUpperCase(), value: element });
+      });
+      return obj;
+    }
+  },
   methods: {
     ...mapActions(["setCategory"])
   }
